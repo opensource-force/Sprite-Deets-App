748852a57cbe3c854faaa6bd748114ffaf721ceb
ejjonny <ejjonny@gmail.com>
2024-12-22 14:38:08 -0700

    cleanup

--- src/components/app-component.ts
+++ src/components/app-component.ts
@@ -1,15 +1,6 @@
-import {
-  Component,
-  StateUpdate,
-  Store,
-  Style,
-  StyleItem,
-} from "../component.ts";
+import { Component, StateUpdate, Store, Style } from "../component.ts";
 import { FileSelect } from "./file-select.ts";
-import { SpriteCanvas } from "./sprite-canvas.ts";
 import { SpriteEditor } from "./sprite-editor.ts";
-import { Timeline } from "./timeline.ts";
-import { Toolbar } from "./toolbar.ts";
 
 export class AppComponent implements Component {
   id = `app-component`;
